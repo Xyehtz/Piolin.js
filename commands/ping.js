1d4aa21e12cd761e5fc1298884d811081de6c235
Xyehtz <alejitogarbo@gmail.com>
2023-05-16 15:56:02 -0500

    Added comments on ping.js

--- commands/ping.js
+++ commands/ping.js
@@ -3,7 +3,6 @@ const { SlashCommandBuilder } = require('discord.js');
 module.exports = {
   // Here we declare the name of the command and the description
   deleted: false,
-<<<<<<< HEAD
   data: new SlashCommandBuilder()
     .setName('ping') 
     .setDescription('Muestra la latencia del bot'), //Shows the bot latency
@@ -16,18 +15,6 @@ module.exports = {
     }); //here we have an early answer from the bot which is loading, thus not leaving the user waiting with no explanation
     const ping = Math.abs(Date.now() - startTimestamp); // Now that some time has passed using math.abs we obtain the ping which is the value resulting of date.now minus starttimestamp
     interaction.editReply(`Latencia: Latencia del Cliente: ${ping}ms`); // Here the bot edits the "loading" message sending a new message, Latency: Client Latency in English
-=======
-  data: new SlashCommandBuilder() 
-    .setName('ping') 
-    .setDescription('Muestra la latencia del bot'), 
-
-  run: async ({ interaction, client, handler }) => {
-    
-    const startTimestamp = Date.now(); 
-    const reply = await interaction.reply({ content: 'loading', fetchReply: true }); 
-    const ping = Math.abs(Date.now() -
-    interaction.editReply(`Latencia: Latencia del Cliente: ${ping}ms`); 
->>>>>>> 6adc248db930f7142dcfc7adcc1b9867d0bcb64f
   },
   devOnly: true, // This declares that this command is only for the developer of the bot
 };
